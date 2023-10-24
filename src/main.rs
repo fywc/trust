f7b4ad176ecb307f500f111d5f941674364d00db
fywc <imfywc@gmail.com>
2023-10-24 22:37:20 +0800

    only parse TCP packet now, and need to redesign TCP format

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,56 @@
 extern crate tun_tap;
 use std::io;
+use std::collections::HashMap;
+
+mod tcp;
+
+#![derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dst: (Ipv4Addr, u16),
+}
 
 fn main() -> io::Result<()>{
+    let mut connections: HashMap<Quad, tcp::State> = Default::default();
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("failed to create tun_tap instance");
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        eprintln!("read flags: {:x?}", &buf[..2]);
-        eprintln!("read proto: {:x?}", &buf[2..4]);
-        eprintln!("read {} bytes: {:x?}", nbytes - 4, &buf[4..nbytes]);
+        let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
+            // no ipv4
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                if proto != 0x06 {
+                    // not tcp
+                    continue;
+                }
+
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4+p.slice().len()..]) {
+                    Ok(p) => {
+                        eprintln!("{} -> {} {}b of tcp to port {}",
+                            src,
+                            dst,
+                            p.slice().len(),
+                            p.destination_port()
+                        );
+                    }
+                    Err(e) => {
+                        eprintln!("Ignoring weird tcp packet {:?}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("ignoring weird packet {:?}", e);
+            }
+        }
     }
     Ok(())
 }
